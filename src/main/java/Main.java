9ca809531cbc0fe67a7f750abe750f91416cc27f
Bogdan <cercel147@gmail.com>
2020-02-10 18:17:53 +0100

    Fuck op

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+        System.out.println("Welcome to Software Design OEF");
     }
 }
