586855ff9935f383fd8ce0d630a17f7153a404a8
Dimitar Georgiev <dsgeorgiev90@yahoo.com>
2020-02-11 16:55:24 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+
+        System.out.println("Welcome all");
+
     }
 }
