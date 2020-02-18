b04ae0261f17c3624e22306a8927d25085b7aaa1
ds-georgiev <60659565+ds-georgiev@users.noreply.github.com>
2020-02-18 12:52:38 +0100

    Revert "Assignment1"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main (String[] args){
-
-        System.out.println("Welcome all");//dsadsa
+        System.out.println("Welcome to Software Design");
     }
 }
