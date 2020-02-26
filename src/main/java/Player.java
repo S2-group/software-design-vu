a7eb4fa7a04ed78d10b57ca072b8c1591fb2aa93
Elizabeth Mieczkowski <eam356@cornell.edu>
2020-02-26 22:08:24 +0100

    Player class with attributes and getter methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,19 @@
-public class Player {
+import java.util.*;
 
+public class Player {
+    private ArrayList<Object> objects;
+    private String currentLocation;
+
+    public Player(ArrayList<Object> objects, String currentLocation) {
+        this.objects = objects;
+        this.currentLocation = currentLocation;
+    }
+
+    public ArrayList<Object> getObjects() {
+        return objects;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
 }
