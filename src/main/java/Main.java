d85b38249b6c3f5e92efdbe1211f339b5c6f5e68
Gemma Standley <gjs97@cornell.edu>
2020-02-27 13:39:10 +0100

    start main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.File;
+
 public class Main {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+
     public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+
+        System.out.println(ANSI_BLUE + "\n Welcome to the Text Adventure Game. \n Please enter the name of the json file you want to load. \n"
+        + ANSI_RESET);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = in.readLine();
+            File jsonFile = new File(input);
+            if (jsonFile.exists()){
+//                    do stuff here
+            }
+            System.out.println("ok");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
