151b6e2673c7d949edf68587c4da19de638b4393
srowekamp <sam.rowekamp@gmail.com>
2020-02-27 10:41:07 +0100

    Add some of Room class

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,22 @@
+import java.util.*
+
 public class Room {
+    private String name;
+    private HashMap<String, Item> items;
+    private Room[] connectsTo;
+    private String script;
+
+    public Room(String name, Item[] items, Room[] connectsTo, String script) {
+        this.name = name;
+        this.items = items;
+        this.connectsTo = connectsTo;
+        this.script = script;
+    } // constructor
+
+    public String getName() {
+        return this.name;
+    } // get name
+
+    public Item getItem(String itemName) {
+    }
 }
