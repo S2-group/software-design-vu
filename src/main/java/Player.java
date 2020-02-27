7de17b780a123402adab2cb8e97c29629d819409
Elizabeth Mieczkowski <eam356@cornell.edu>
2020-02-27 09:50:31 +0100

    change object keyword to item

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 public class Player {
-    private ArrayList<Object> objects;
+    private ArrayList<Object> inventory;
     private String currentLocation;
 
-    public Player(ArrayList<Object> objects, String currentLocation) {
-        this.objects = objects;
+    public Player(ArrayList<Object> inventory, String currentLocation) {
+        this.inventory = inventory;
         this.currentLocation = currentLocation;
     }
 
-    public ArrayList<Object> getObjects() { return objects; }
+    public ArrayList<Object> getObjects() { return inventory; }
 
     public String getCurrentLocation() { return currentLocation; }
 
