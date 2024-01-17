0a5e531697d66b760acf1429981b5733ed25117d
Eoan O Dea <odeaeoan@gmail.com>
2024-01-17 19:45:30 +0100

    updated print

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -2,6 +2,6 @@ package softwaredesign;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+        System.out.println("Welcome to Software Design New change");
     }
 }
