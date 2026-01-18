c79dc5c219ee8d62db7b95e28cd4957678ddf01f
ShaurGaur <shauryavrat@live.com>
2026-01-18 17:56:03 +0100

    replace Main class with new top-level main method

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,7 +1,3 @@
-package nl.vu.cs.softwaredesign;
-
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design!");
-    }
+void main() {
+    System.out.println("Welcome to Software Design!");
 }
